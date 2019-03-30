e09336ed7124bafe7ee69d004de5ab84531b1ec4
saravind23399 <saravind23399@gmail.com>
2019-03-30 13:40:01 +0530

    Detected Face from Camera

--- app/src/main/java/com/mepco/mepcoauth/MainActivity.java
+++ app/src/main/java/com/mepco/mepcoauth/MainActivity.java
@@ -1,15 +1,72 @@
 package com.mepco.mepcoauth;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+import android.util.SparseArray;
 import android.widget.Toast;
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final int requestCode = 20;
+    private ImageView imageHolder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show();
+        Button btnClick = findViewById(R.id.btnClick);
+        imageHolder = findViewById(R.id.imgCamPic);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(photoCaptureIntent, requestCode);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(this.requestCode == requestCode && resultCode == RESULT_OK){
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            Paint myRectPaint = new Paint();
+            myRectPaint.setStrokeWidth(5);
+            myRectPaint.setColor(Color.RED);
+            myRectPaint.setStyle(Paint.Style.STROKE);
+            Bitmap tempBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.RGB_565);
+            Canvas tempCanvas = new Canvas(tempBitmap);
+            tempCanvas.drawBitmap(bitmap, 0, 0, null);
+            FaceDetector faceDetector = new
+                    FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
+                    .build();
+            if(!faceDetector.isOperational()){
+                Toast.makeText(this, "Cannot Setup Face Detector", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<Face> faces = faceDetector.detect(frame);
+                Face thisFace = faces.valueAt(1);
+                float x1 = thisFace.getPosition().x;
+                float y1 = thisFace.getPosition().y;
+                float x2 = x1 + thisFace.getWidth();
+                float y2 = y1 + thisFace.getHeight();
+                tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
+            imageHolder.setImageDrawable(new BitmapDrawable(getResources(),tempBitmap));
+        }
     }
 }
