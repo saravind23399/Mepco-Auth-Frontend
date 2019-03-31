4eb03ad7c92c06ea8c13388f0e10e24a4410a63b
saravind23399 <saravind23399@gmail.com>
2019-03-31 12:38:19 +0530

    Improved Image Quality and Added Permissions

--- app/src/main/java/com/mepco/mepcoauth/MainActivity.java
+++ app/src/main/java/com/mepco/mepcoauth/MainActivity.java
@@ -1,72 +1,145 @@
 package com.mepco.mepcoauth;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
-import android.util.SparseArray;
-import android.widget.Toast;
-import android.app.AlertDialog;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.RectF;
-import android.graphics.drawable.BitmapDrawable;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private final int requestCode = 20;
     private ImageView imageHolder;
+    Uri image;
+    String mCameraFileName;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnClick = findViewById(R.id.btnClick);
         imageHolder = findViewById(R.id.imgCamPic);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            btnClick.setText("Allow Permssions");
+        }
+
         btnClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(photoCaptureIntent, requestCode);
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA}, 2);
+                }
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+                }
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 2);
+                }
+
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                    StrictMode.setVmPolicy(builder.build());
+                    Intent intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    Date date = new Date();
+                    DateFormat df = new SimpleDateFormat("-mm-ss");
+
+                    String newPicFile = df.format(date) + ".jpg";
+                    String outPath = "/sdcard/" + newPicFile;
+                    File outFile = new File(outPath);
+
+                    mCameraFileName = outFile.toString();
+                    Uri outuri = Uri.fromFile(outFile);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, outuri);
+                    startActivityForResult(intent, 2);
+                }
             }
         });
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(this.requestCode == requestCode && resultCode == RESULT_OK){
-            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-            Paint myRectPaint = new Paint();
-            myRectPaint.setStrokeWidth(5);
-            myRectPaint.setColor(Color.RED);
-            myRectPaint.setStyle(Paint.Style.STROKE);
-            Bitmap tempBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.RGB_565);
-            Canvas tempCanvas = new Canvas(tempBitmap);
-            tempCanvas.drawBitmap(bitmap, 0, 0, null);
-            FaceDetector faceDetector = new
-                    FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
-                    .build();
-            if(!faceDetector.isOperational()){
-                Toast.makeText(this, "Cannot Setup Face Detector", Toast.LENGTH_SHORT).show();
-                return;
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 2) {
+                if (data != null) {
+                    image = data.getData();
+                }
+                if (image == null && mCameraFileName != null) {
+                    image = Uri.fromFile(new File(mCameraFileName));
+                }
+                Bitmap bitmap = BitmapFactory.decodeFile(image.getPath());
+                ExifInterface exif;
+                try {
+                    exif = new ExifInterface(image.getPath());
+                } catch (IOException e) {
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
+
+                Matrix matrix = new Matrix();
+                matrix.preRotate(270);
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+
+                imageHolder.setImageBitmap(bitmap);
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inMutable = true;
+                FaceDetector faceDetector = new
+                        FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
+                        .build();
+                if (!faceDetector.isOperational()) {
+                    Toast.makeText(this, "Error in Face Detector", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                SparseArray<Face> faces = faceDetector.detect(frame);
+                Face thisFace;
+                new File(image.getPath()).delete();
+                try {
+                    thisFace = faces.valueAt(0);
+                } catch (Exception e) {
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                int x1 = (int) thisFace.getPosition().x;
+                int y1 = (int) thisFace.getPosition().y;
+                int x2 = (int) thisFace.getWidth();
+                int y2 = (int) thisFace.getHeight();
+                Bitmap bMap = Bitmap.createBitmap(bitmap, x1, y1, x2, y2);
+                imageHolder.setImageBitmap(bMap);
+                findViewById(R.id.btnClick).setEnabled(false);
+
             }
-            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-            SparseArray<Face> faces = faceDetector.detect(frame);
-                Face thisFace = faces.valueAt(1);
-                float x1 = thisFace.getPosition().x;
-                float y1 = thisFace.getPosition().y;
-                float x2 = x1 + thisFace.getWidth();
-                float y2 = y1 + thisFace.getHeight();
-                tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
-            imageHolder.setImageDrawable(new BitmapDrawable(getResources(),tempBitmap));
         }
     }
 }
